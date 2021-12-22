9339f58031525498a8210dbd7cfb45f0f6d300a9
Anil Goyal <anilrini@gmail.com>
2021-12-22 12:33:16 +0530

    day21

--- Day8/day_8.R
+++ Day8/day_8.R
@@ -1,3 +1,10 @@
 library(tidyverse)
 
 input <- read_lines('Day8\\input8.txt')
+
+input[1] %>% as_tibble() %>% 
+  separate(value, into = c('inputs', 'outputs'), sep = ' \\| ') %>% 
+  mutate(across(everything(), ~str_split(., ' '))) %>% 
+  mutate(across(.fns =  ~ map(., function(.z) str_split(.z, ''))),
+         across(everything(), ~ map(., function(.a) map(.a, function (.b) length(.b))), 
+                .names = 'len_{.col}')) %>% str
