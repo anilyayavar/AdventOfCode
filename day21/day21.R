67fde436cd4f13aa0a11733a72df0f78f1d0aa03
Anil Goyal <anilrini@gmail.com>
2021-12-22 12:32:04 +0530

    day21

---
+++ day21/day21.R
@@ -0,0 +1,25 @@
+#lets define a function
+day21_1 <- function(init){
+  n <- 1
+  
+  p1 <- 0
+  p2 <- 0
+  
+  while(p1 < 1000 && p2 < 1000){
+    moves <- (((6 + 9 * (seq(n)-1)) ) %% 10 )
+    score <- reduce(map2(split(moves, seq(n) %% 2 == 0), init, 
+                         ~ {p <- (.y + 1 + (cumsum(.x) -1 ) %% 10); 
+                         f <- (1 + (p -1) %% 10); 
+                         cumsum(f) }), 
+                    function(.a, .b) c(rbind(.a, .b)))
+    if (n %% 2 == 0){
+      p1 <- score[n]
+    } else {
+      p2 <- score[n]
+    }
+    if (p1 >= 1000 | p2 >= 1000) print(3 * n * score[n-1])
+    n <- n + 1
+  }
+}
+# for sample
+day21_1(c(4,8))
