4bb978bae002baaf3c8f9e0701f7af05f3671393
Anil Goyal <anilrini@gmail.com>
2022-02-16 21:24:13 +0530

    Day 2 part2

--- Previous Years/2019/Solutions2019.R
+++ Previous Years/2019/Solutions2019.R
@@ -54,3 +54,40 @@ answer2_1 <- function(x){
 answer2_1(input)
 
 ## part-2
+
+
+answer2_2 <- function(input){
+  # set x
+  mat <- matrix(rep(0, 100*100), nrow = 100)
+  x <- input
+  for(j in seq(100) -1){
+    for(k in seq(100)-1){
+      
+      #set initial values
+      x[2] <- j
+      x[3] <- k
+      # set i (iteration)
+      i <- 1
+      # while loop
+      while(TRUE){
+        if(x[4*(i-1)+1] ==99){
+          break
+        } else if (x[4*(i-1)+1] == 1){
+          x[x[4*i] +1] <- x[x[4*(i-1)+2] +1] + x[x[4*(i-1)+3] +1]
+        } else {
+          x[x[4*i] +1] <- x[x[4*(i-1)+2] +1] * x[x[4*(i-1)+3] +1]
+        }
+        i <- i +1
+      }
+      # input mat value
+      mat[j+1, k+1] <- x[1]
+      # reset x
+      x <- input
+      
+    }
+  }
+  # final answer
+  100*(which(mat == 19690720, arr.ind = TRUE)[1]-1)+(which(mat == 19690720, arr.ind = TRUE)[2]-1)
+}
+
+answer2_2(input)
