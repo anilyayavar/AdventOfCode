bca7913cd4cc73caa3caedfe4320822cad1f2f99
Anil Goyal <anilrini@gmail.com>
2022-02-16 20:28:09 +0530

    day2 part1

--- Previous Years/2019/Solutions2019.R
+++ Previous Years/2019/Solutions2019.R
@@ -26,3 +26,31 @@ answer1_2 <- function(input){
 answer1_2(input)
 
 #### ............DAY 2 ...........####
+
+input <- readLines('Previous Years/2019/input2.txt') %>% str_split(',') %>% unlist %>% as.integer()
+
+## part-1
+answer2_1 <- function(x){
+  #set initial values
+  x[2] <- 12
+  x[3] <- 2
+  # set i (iteration)
+  i <- 1
+  # while loop
+  while(TRUE){
+    if(x[4*(i-1)+1] ==99){
+      break
+    } else if (x[4*(i-1)+1] == 1){
+      x[x[4*i] +1] <- x[x[4*(i-1)+2] +1] + x[x[4*(i-1)+3] +1]
+    } else {
+      x[x[4*i] +1] <- x[x[4*(i-1)+2] +1] * x[x[4*(i-1)+3] +1]
+    }
+    i <- i +1
+  }
+  # final answer
+  x[1]
+}
+
+answer2_1(input)
+
+## part-2
