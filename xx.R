d964684da79b6ef18cc59fb0ccef2b6173508b2e
Anil Goyal <anilrini@gmail.com>
2022-03-16 17:02:58 +0530

    earlier stars

--- xx.R
+++ xx.R
@@ -2,20 +2,4 @@ library(tidyverse)
 
 input <- read_lines('Previous Years/2020/input18.txt')
 
-# define helper functions
-`%a%` <- function(x,y) x+y
-`%s%` <- function(x,y) x-y
-`%m%` <- function(x,y) x*y
-`%d%` <- function(x,y) x/y
-
-# modify input
-input <- reduce2(list('\\+', '\\-', '\\*', '\\/'), list('%a%', '%s%', '%m%', '%d%'),
-        .init = input,
-        function(.x, .y, .z) map(.x, function(.a) str_replace_all(.a, .y, .z )))
-
-# final answer
-map_dbl(input, ~ eval(parse(text = .x))) %>% 
-  sum()
-
-
 
