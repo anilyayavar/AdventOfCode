dbb513177f29ef6170f49f3a52d84dfa18aa9964
Anil Goyal <anilrini@gmail.com>
2022-12-09 13:21:49 +0530

    day3_2

--- 2022/Script file.R
+++ 2022/Script file.R
@@ -6,12 +6,5 @@ library(tidyverse)
 input <- readLines('2022/input3.txt')
 
 
-  map2_chr(
-    str_sub(input, start = 1, end = nchar(input)/2),
-    str_sub(input, start = (nchar(input)/2)+1, end = -1L),
-    ~ intersect(unlist(str_split(.x, '')), unlist(str_split(.y, '')))
-  ) %>% 
-    {sum(match(., letters), na.rm = T) + sum(match(., LETTERS) +26, na.rm = T)}
-
 
   
