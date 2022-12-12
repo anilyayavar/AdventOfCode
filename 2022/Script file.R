0dea08f3d82aa3609f67573301f8d703815e559b
Anil Goyal <anilrini@gmail.com>
2022-12-12 10:39:48 +0530

    Day4

--- 2022/Script file.R
+++ 2022/Script file.R
@@ -3,8 +3,14 @@ library(tidyverse)
 
 ############### day-2
 
-input <- readLines('2022/input3.txt')
+input <- readLines('2022/input4.txt')
 
+input %>% str_split(',') %>% 
+  map(~ str_split(.x, '-') %>% unlist() %>% as.integer()) %>% 
+  map_lgl(~ ((.x[1] <= .x[3]) & (.x[2] >= .x[3])) | ( (.x[1] <= .x[4]) & (.x[2] >= .x[4]) ) ) %>% 
+  sum()
 
+length(input)
 
+1000-32
   
