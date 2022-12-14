99d290e9d9088fe48a592e4fff4f298b8963d182
Anil Goyal <anilrini@gmail.com>
2022-12-14 14:30:00 +0530

    Day-12

--- 2022/Script file.R
+++ 2022/Script file.R
@@ -1,16 +1,57 @@
 library(tidyverse)
+library(igraph)
+library(magrittr)
+library(visNetwork)
+
+g <- make_ring(10)
+distances(g)
+shortest_paths(g, 5, 9) %$% vpath[[1]]
+all_shortest_paths(g, 1, 6:8)
+mean_distance(g)
+plot(g)
 
 
-############### day-2
+############### day-12
 
-input <- readLines('2022/input4.txt')
+input <- readLines('2022/input12.txt')
+len <- nchar(input[1])
+input %>% 
+  str_split('') %>% 
+  unlist() %>% 
+  {seq_along(.) -1} %>% 
+  {(. %% len)+(. %/% len)*(0+1i)} -> input_nodes
 
-input %>% str_split(',') %>% 
-  map(~ str_split(.x, '-') %>% unlist() %>% as.integer()) %>% 
-  map_lgl(~ ((.x[1] <= .x[3]) & (.x[2] >= .x[3])) | ( (.x[1] <= .x[4]) & (.x[2] >= .x[4]) ) ) %>% 
-  sum()
+names(input_nodes) <- input %>% 
+  str_split('') %>% 
+  unlist()
 
-length(input)
+input_nodes1 <- input_nodes
+from_node <- input_nodes1[names(input_nodes1) == 'S']
+to_node <- input_nodes1[names(input_nodes1) == 'E']
+from_nodes <- input_nodes1[names(input_nodes1) == 'a']
 
-1000-32
-  
+names(input_nodes) <- ifelse(names(input_nodes) == 'S', 
+                             'a', 
+                             ifelse(names(input_nodes) == 'E', 
+                                    'z',
+                                    names(input_nodes)
+                                    )
+                             )
+
+names(input_nodes) <- match(names(input_nodes), letters)
+
+cross2(input_nodes, input_nodes, .filter = function(x, y) (Mod(x-y)>1 | x==y) ) %>% 
+  map_df(setNames, c('from', 'to')) %>% 
+  filter(as.integer(names(input_nodes)[match(to, input_nodes)]) - as.integer(names(input_nodes)[match(from, input_nodes)]) <= 1) %>% 
+  graph_from_data_frame() -> gg
+
+from_g <- V(gg) %>% names() %>% as.complex() %>% {which(. == from_node)}
+to_g <- V(gg) %>% names() %>% as.complex() %>% {which(. == to_node)}
+
+distances(gg, v = from_g, to_g, mode = 'out', algorithm = 'unweighted')
+
+from_g2 <- V(gg) %>% names() %>% as.complex() %>% {which(. %in% from_nodes)}
+
+distances(gg, v = from_g2, to_g, mode = 'out', algorithm = 'unweighted') %>% min()
+
+
