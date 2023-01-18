79c4e83e9f4d3c89726ae86aa3f174ec2d095d54
Anil Goyal <anilrini@gmail.com>
2023-01-18 16:31:20 +0530

    Day-25

--- 2022/Script file.R
+++ 2022/Script file.R
@@ -1,48 +1,36 @@
 library(tidyverse)
-readLines('2022/input1.txt') |> 
-  as.numeric() |> 
-  as.data.frame() |> 
-  set_names('input') |> 
-  group_by(grp = cumsum(is.na(input))) |> 
-  summarise(food = sum(input, na.rm = T)) |> 
-  slice_max(order_by = food)
+x <- '12111'
 
-readLines('2022/input1.txt') |> 
-  as.numeric() |> 
-  as.data.frame() |> 
-  set_names('input') |> 
-  group_by(grp = cumsum(is.na(input))) |> 
-  summarise(food = sum(input, na.rm = T)) |> 
-  slice_max(order_by = food, n=3) |> 
-  summarise(sum(food))
-
-############### day-2
-
-
-readLines('2022/input2.txt') |> 
-  as_tibble() |> 
-  set_names('turns') |> 
-  separate(turns, into = c('Me', "You"))
-
-
-fruits <- c(
-  "apples and oranges and pears and bananas",
-  "pineapples and mangos and guavas"
-)
-
-str_split('nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg', '') |> 
-  unlist() -> samp
-
-for(i in seq_along(samp)){
-  if(length(unique(samp[i:i+3]))==4){
-    break
-  }
+x
+snafu <- function(x){
+  str_split(x, '') %>% 
+    unlist() %>% 
+    rev() %>% 
+    {case_when(. == '-' ~ '-1',
+               . == '=' ~ '-2',
+               TRUE ~ .)} %>% 
+    as.integer() %>% 
+    imap(~ .x * (5^(.y-1))) %>% 
+    unlist() %>% 
+    sum()
 }
 
-input <- readLines('2022/input6.txt') |> str_split('') |> unlist()
+input <- readLines('2022/input25.txt')
 
-i <- 1
-while (length(unique(input[i:(i+13)])) < 14) {
-  i <- i + 1  
-}
-i+13
+map(input, snafu) %>% 
+  unlist() 
+
+snafu(input[3])
+install.packages('gmp')
+
+x <- input[3]
+str_split(x, '') %>% 
+  unlist() %>% 
+  rev() %>% 
+  {case_when(. == '-' ~ '-1',
+             . == '=' ~ '-2',
+             TRUE ~ .)} %>% 
+  as.integer() %>% 
+  imap(~ .x * (5^(.y-1))) %>% 
+  unlist() %>% 
+  sum()
