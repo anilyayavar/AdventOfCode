396c99d93494b10e428bcba13cd46a0f29e4beb6
Anil Goyal <anilrini@gmail.com>
2023-08-22 15:14:49 +0530

    Update Script file.R

--- 2022/Script file.R
+++ 2022/Script file.R
@@ -1,36 +1,9 @@
 library(tidyverse)
-x <- '12111'
 
-x
-snafu <- function(x){
-  str_split(x, '') %>% 
-    unlist() %>% 
-    rev() %>% 
-    {case_when(. == '-' ~ '-1',
-               . == '=' ~ '-2',
-               TRUE ~ .)} %>% 
-    as.integer() %>% 
-    imap(~ .x * (5^(.y-1))) %>% 
-    unlist() %>% 
-    sum()
-}
+install.packages('here')
+library(here)
 
-input <- readLines('2022/input25.txt')
+here('2022')
 
-map(input, snafu) %>% 
-  unlist() 
 
-snafu(input[3])
-install.packages('gmp')
 
-x <- input[3]
-str_split(x, '') %>% 
-  unlist() %>% 
-  rev() %>% 
-  {case_when(. == '-' ~ '-1',
-             . == '=' ~ '-2',
-             TRUE ~ .)} %>% 
-  as.integer() %>% 
-  imap(~ .x * (5^(.y-1))) %>% 
-  unlist() %>% 
-  sum()
