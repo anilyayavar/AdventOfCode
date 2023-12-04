85b465459f4bd7fd42b58a03285f29a10b780fe3
Anil Goyal <anilrini@gmail.com>
2023-12-04 14:59:58 +0530

    Day-4

--- 2023/Day1.R
+++ 2023/Day1.R
@@ -1,10 +1,7 @@
 library(tidyverse)
-str_extract_all("xabc1nhg2bv5", pattern = "\\d+")
-str_c
-
-
+library(stringi)
 input1 <- readLines('2023/data/input1.txt')
-
+# Part-1
 input1 %>% 
   as_tibble() %>% 
   set_names('calibration') %>% 
@@ -15,4 +12,18 @@ input1 %>%
   summarise(ans = sum(cal_val)) %>% 
   pull(ans)
 
-         
+# Part-2
+digits <- c("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+digit <- c(set_names(1:9, digits), set_names(1:9, 1:9))
+
+input1 %>% 
+  as_tibble() %>% 
+  set_names('calibration') %>% 
+  mutate(val = map(calibration, ~ str_extract_all(.x, paste0(c(digits, "\\d"), collapse = "|")) %>% unlist()),
+         val1 = digit[map_chr(val, first)],
+         val = map(stri_reverse(calibration), ~ str_extract_all(.x, paste0(c(stri_reverse(digits), "\\d"), collapse = "|")) %>% unlist()),
+         val2 = digit[stri_reverse(map_chr(val, first))]) %>% 
+  mutate(cal_val = val1*10+val2) %>% 
+  summarise(ans = sum(cal_val)) %>% 
+  pull(ans)
+
---
+++ 2023/Day4.R
@@ -0,0 +1,52 @@
+library(tidyverse)
+library(unglue)
+
+
+input4 <- readLines("2023/data/input4.txt")
+
+patterns <- "Card{=\\s+}{card}: {wins} | {draws}"
+
+input4[1]
+unglue_data(input4, patterns = patterns) %>% 
+  mutate(wins = str_extract_all(wins, "\\d+"),
+         wins = map(wins, as.integer),
+         draws = str_extract_all(draws, "\\d+"),
+         draws = map(draws, as.integer),
+         score = map2_int(draws, wins,
+                         ~ sum(.x %in% .y)),
+         score = ifelse(score == 0, 0, 2^(score-1))) %>% 
+  summarise(ans = sum(score))
+
+
+## Part-2
+d <- unglue_data(input4, patterns = patterns) %>% 
+  mutate(wins = str_extract_all(wins, "\\d+"),
+         wins = map(wins, as.integer),
+         draws = str_extract_all(draws, "\\d+"),
+         draws = map(draws, as.integer),
+         score = map2_int(draws, wins,
+                          ~ sum(.x %in% .y))) 
+
+ans <- 0
+seqi <- c()
+
+for(i in 1:220){
+  seqi <- c(i, seqi)
+  card_nums <- sum(i == seqi)
+  seq_n <- seq(from = i + 1L, length.out = d$score[i])
+  seq_n <- rep(seq_n, card_nums)
+  seqi <- c(seqi, seq_n)
+  ans <- ans + card_nums
+} 
+ans
+
+
+
+
+
+
+i
+i == seqi
+seq_n
+
+seq(from = 5, length.out = 1)
